4e878cd106792879ad8de8794c2a907fe1e7bdd0
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-20 19:53:18 -0800

    created Signup page

---
+++ app/routes/signup.tsx
@@ -0,0 +1,85 @@
+import { ActionArgs, ActionFunction, json, LinksFunction, redirect } from "@remix-run/node";
+import { Link, useActionData, useSearchParams } from "@remix-run/react";
+// import { createUser } from "~/model/user.server";
+
+import stylesUrl from "~/styles/login.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
+
+type ActionData =
+  | {
+      username: null | string;
+      password: null | string;
+    }
+  | undefined;
+
+function validateUsername(username: unknown) {
+    if (typeof username !== "string" || username.length < 3) {
+      return `Usernames must be at least 3 characters long`;
+    }
+  }
+  
+  function validatePassword(password: unknown) {
+    if (typeof password !== "string" || password.length < 6) {
+      return `Passwords must be at least 6 characters long`;
+    }
+  }
+  
+  function validateUrl(url: string) {
+    let urls = ["/jokes", "/", "https://remix.run"];
+    if (urls.includes(url)) {
+      return url;
+    }
+    return "/jokes";
+  }
+
+// export const action:ActionFunction=async({request} : ActionArgs)=>{
+//    const form = await request.formData()
+//    const name = form.get('username')
+//    const password = form.get('password')
+//    const AnyMissingDataError = {
+//     username: name ? null : "name is reqired",
+//     password: password ? null : "password is reqired",
+//   };
+//   if (Object.values(AnyMissingDataError).some((el) => el)) {
+//     return json<ActionData>(AnyMissingDataError); }
+//    await createUser({username:name , passwordHash:password})
+//    return redirect('..')
+// }
+
+export default function Login() {
+  const [searchParams] = useSearchParams();
+  const AnyMissingDataError = useActionData() as ActionData;
+  return (
+    <div className="container">
+      <div className="content" data-light="">
+        <h1 style={{fontSize:37 , color: "#497174"}}>Sign up</h1>
+        <form method="post">
+          <div>
+            <label htmlFor="username-input">
+              Username{" "}
+              {AnyMissingDataError?.username ? (
+                <em style={{ color: "red" }}>{AnyMissingDataError.username}</em>
+              ) : null}
+            </label>
+            <input type="text" id="username-input" name="username" />
+          </div>
+          <div>
+            <label htmlFor="password-input">
+              Password
+              {AnyMissingDataError?.password ? (
+                <em style={{ color: "red" }}>{AnyMissingDataError.password}</em>
+              ) : null}
+            </label>
+            <input id="password-input" name="password" type="password" />
+          </div>
+          <button type="submit" className="button">
+            Sign up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
