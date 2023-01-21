25431a7ffa83b0e41176c3b8f53e021f12c17be9
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-20 23:43:13 -0800

    Added design for task page

--- app/model/task.server.ts
+++ app/model/task.server.ts
@@ -1,8 +1,24 @@
 import { PrismaClient } from "@prisma/client";
+import { getUserId } from "./user.server";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export function getTasks()
-{
-    return prisma.task.findMany();
+export async function getTasks(request) {
+  return prisma.task.findMany({
+    where: { id: await getUserId(request) },
+    take: 5,
+    orderBy: { createdAt: "desc" },
+    select: { id: true, title: true },
+  });
+}
+export async function getColumn() {
+  return prisma.columns.findMany();
+}
+export async function getToDoTasks(request) {
+    return prisma.task.findMany({
+        where: { UserId: await getUserId(request)  , ColumnsId:"6c17b236-cfbc-418c-9883-9f737ec8065b" },
+        take: 5,
+        orderBy: { createdAt: "desc" },
+        select: { id: true, title: true },
+      });
 }
--- app/routes/tasks.tsx
+++ app/routes/tasks.tsx
@@ -1,33 +1,68 @@
-import { PrismaClient } from '@prisma/client'
-import { LoaderArgs } from '@remix-run/node'
-import { Link } from '@remix-run/react'
-import React from 'react'
-import { json, LoaderFunction, useLoaderData } from 'react-router'
-import { getTasks } from '~/model/task.server'
-import { getUser, getUserId } from '~/model/user.server'
-export  const loader = async({request}:LoaderArgs)=>{
-    const db = new PrismaClient()
-    console.log('user' , await getUserId(request))
-    const tasksListItems = await getTasks(request)
-    const user = await getUser(request);
-  
-    return json({
-      tasksListItems,
-      user,
-    });
-}
-export default function tasks(){
-    const data = useLoaderData();
-    return (
-        <div>
-           <form action="/logout" method="post">
-                <button type="submit" className="button">
-                  Logout
-                </button>
-              </form>
-           <div>
-            {data.user.username}
-           </div>
+import { PrismaClient } from "@prisma/client";
+import { LinksFunction, LoaderArgs } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+import React from "react";
+import { json, LoaderFunction, useLoaderData } from "react-router";
+import { getColumn, getTasks, getToDoTasks } from "~/model/task.server";
+import { getUser, getUserId } from "~/model/user.server";
+import stylesUrl from "~/styles/task.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export const loader = async ({ request }: LoaderArgs) => {
+  console.log("user", await getUserId(request));
+  const tasksListItems = await getTasks(request);
+  const user = await getUser(request);
+  const columns = await getColumn();
+  const toDo = await getToDoTasks(request);
+
+  return json({
+    tasksListItems,
+    user,
+    toDo,
+  });
+};
+export default function tasks() {
+  const data = useLoaderData();
+  return (
+    <div className="task-layout">
+      <header>
+        <h2>
+          <Link to="/tasks">
+            <span className="title">Swapy</span>
+          </Link>
+        </h2>
+        <div className="welcome">
+          <span className="Hi">{`Hi ${data.user.username}`}</span>
+          <form action="/logout" method="post" className="logout-form">
+            <button type="submit" className="button">
+              Logout
+            </button>
+          </form>
         </div>
-    )
+      </header>
+      <main className="tasks-main">
+        <div className="main-child">
+          <h2>to Do</h2>
+          {data.toDo.map((t) => (
+            <div className="single-task">{t.title}</div>
+          ))}
+        </div>
+        <div className="main-child">
+          <h2>Doing</h2>
+          {data.toDo.map((t) => (
+            <div className="single-task">{t.title}</div>
+          ))}
+        </div>
+        <div className="main-child">
+          <h2>Done</h2>
+          {data.toDo.map((t) => (
+            <div className="single-task">{t.title}</div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
