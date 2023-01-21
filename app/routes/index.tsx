0c52f4942fa9af71a15ac8ff4418dd4f1c08631c
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-01-20 19:31:59 -0800

    Added style for home page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,23 @@
+import { LinksFunction } from "@remix-run/node";
+import { Link } from "react-router-dom";
+import stylesUrl from "~/styles/global.css";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container">
+    <div className="title-container">
+      <h1>
+        Swapy
+      </h1>
+      <h2>See your tasks done</h2>
+    </div>
+    <div className="btn-container">
+      <Link to={'signin'} className="signin-btn">SignIn</Link>
+    </div>
     </div>
   );
 }
